00b0d94f85a62e23688a1d4a43a9bcfac9c61af8
tito10047 <jozef@mostka.sk>
2024-05-28 20:38:26 +0200

    add initial line to stack trace

---
+++ src/Service/ErrorFormater.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jozef Môstka
+ * Date: 28. 5. 2024
+ * Time: 20:21
+ */
+namespace BugCatcher\Reporter\Service;
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Utils;
+use Throwable;
+
+class ErrorFormater extends LineFormatter {
+
+	protected function normalizeException(Throwable $e, int $depth = 0): string {
+		$stackTrace = $e->getTraceAsString();
+		$stackTrace = "#0 " . $e->getFile() . "(" . $e->getLine() . "): " . $e->getMessage() . "\n" . $stackTrace;
+
+		return $stackTrace;
+	}
+}
